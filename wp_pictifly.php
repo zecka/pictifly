ae8c2efcbc9b47d0e763aa3ec4b40ff934872959
Robin Ferrari <ferrari.robin@gmail.com>
2019-10-04 10:02:38 +0200

    Github updater compatibiliy

--- wp_pictifly.php
+++ wp_pictifly.php
@@ -2,14 +2,16 @@
 /*
 Plugin Name: Pictifly
 Plugin URI:
+GitHub Plugin URI: zecka/pictifly
+GitHub Plugin URI: https://github.com/zecka/pictifly
 Description: A plugin to generate image size on the fly and create picture html tag
 Author: Robin Ferrari (Octree)
 Author URI: https://octree.ch
 Text Domain: pictifly
-Version: 0.1
+Version: 0.1.1
 */
 
-define( 'PF_VERSION', '0.1' );
+define( 'PF_VERSION', '0.1.1' );
 define( 'PF_URL', substr(plugin_dir_url( __FILE__ ), 0, -1) );
 define( 'PF_PATH', substr(plugin_dir_path( __FILE__ ), 0, -1) );
 
