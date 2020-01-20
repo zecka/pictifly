ac45e38de45f60376c671fc2efa01105315ba1bc
Robin Ferrari <ferrari.robin@gmail.com>
2020-01-20 18:04:07 +0100

    Move script in footer

--- kernel/enqueue_scripts.php
+++ kernel/enqueue_scripts.php
@@ -6,9 +6,9 @@ function pf_enqueue_scripts(){
     wp_register_style( 'pf_style', PF_URL .$mystyle, '', filemtime(PF_PATH.$mystyle));
     wp_enqueue_style('pf_style');
 
-    wp_register_script( 'lazysizes', PF_URL . '/assets/vendors/js/lazysizes.min.js' ,array( 'jquery'),'4.1.15');
+    wp_register_script( 'lazysizes', PF_URL . '/assets/vendors/js/lazysizes.min.js' ,array( 'jquery'),'4.1.15', true);
 
-    wp_register_script( 'pf_scripts', PF_URL . '/assets/js/op.js' ,array( 'jquery'),'4.1.15');
+    wp_register_script( 'pf_scripts', PF_URL . '/assets/js/op.js' ,array( 'jquery'),'4.1.15', true);
     $pf_configs = pf_configs();
     wp_localize_script('pf_scripts', 'pf_breakpoints', $pf_configs['breakpoints']);
     
