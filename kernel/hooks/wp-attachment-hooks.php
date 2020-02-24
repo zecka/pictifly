f4cf81cfafc6e2241efae27cae1485dc6c2b3874
Robin Ferrari <ferrari.robin@gmail.com>
2020-02-24 08:46:29 +0100

    Fix bug on remove attachment

--- kernel/hooks/wp-attachment-hooks.php
+++ kernel/hooks/wp-attachment-hooks.php
@@ -6,7 +6,7 @@ function pf_on_delete_attachement( $post_id ) {
 	// pf_delete_pictifly_files($post_id, 'pf_files_min' );
 }
 function pf_delete_pictifly_files($post_id){
-	pf_remove_attachment_dir($post_id);
+	PF_Helper::pf_remove_attachment_dir($post_id);
 }
 // add the action
 add_action( 'delete_attachment', 'pf_on_delete_attachement', 10, 1 );
