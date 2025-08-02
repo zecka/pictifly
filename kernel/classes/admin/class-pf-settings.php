a94d0ae5d6dfd58736bc415a4340daa65edc36a7
ZecKa <18116930+zecka@users.noreply.github.com>
2025-08-02 17:21:20 +0200

    fix: PF_Settngs init

--- kernel/classes/admin/class-pf-settings.php
+++ kernel/classes/admin/class-pf-settings.php
@@ -16,17 +16,18 @@ class PF_Settings {
      * @return void
      */
     private function __construct() {
-        $this->set_options();
         $this->register_ajax_action();
     }
-    
+    public function init() {
+        $this->set_options();
+    }
     private function set_options() {
         $options = get_option('pictifly_settings');
         $options = (is_array($options)) ? $options : [];
         $default_options = PF_Helper::default_settings();
         // override default option with saved options
         $options = array_merge($default_options, $options);
-        $this->options = apply_filters( 'pf_options', $options );
+        $this->options = apply_filters('pf_options', $options);
     }
 
     /**
@@ -41,6 +42,7 @@ class PF_Settings {
         if (is_null(self::$_instance)) {
             self::$_instance = new PF_Settings();
         }
+        self::$_instance->init();
 
         return self::$_instance;
     }
@@ -50,9 +52,9 @@ class PF_Settings {
     }
     public function save_plugin_settings() {
         $imgix_url = esc_url_raw($_POST['imgix_url']);
-        if($imgix_url){
+        if ($imgix_url) {
             $imgix = $_POST['imgix'] === "true";
-        }else{
+        } else {
             $imgix = false;
         }
         $new_options = [
@@ -64,18 +66,18 @@ class PF_Settings {
         ];
 
         // Check if quality is possible
-        if($new_options['pictifly_quality'] > 100 || $new_options['pictifly_quality'] < 0){
+        if ($new_options['pictifly_quality'] > 100 || $new_options['pictifly_quality'] < 0) {
             $new_options['pictifly_quality'] = 100;
         }
-        if($new_options['compression_quality'] > 100 || $new_options['compression_quality'] < 0){
+        if ($new_options['compression_quality'] > 100 || $new_options['compression_quality'] < 0) {
             $new_options['compression_quality'] = 100;
         }
 
         // check if compression quality have change
-        if($this->options['compression_quality'] !== $new_options['compression_quality']){
+        if ($this->options['compression_quality'] !== $new_options['compression_quality']) {
             PF_Tools::delete_compressed_images();
         }
-        if($this->options['pictifly_quality'] !== $new_options['pictifly_quality']){
+        if ($this->options['pictifly_quality'] !== $new_options['pictifly_quality']) {
             PF_Tools::delete_all_pictifly_images();
         }
 
@@ -84,7 +86,6 @@ class PF_Settings {
 
 
 
-        wp_send_json_success(['options'=>$new_options]);
+        wp_send_json_success(['options' => $new_options]);
     }
-
 }
